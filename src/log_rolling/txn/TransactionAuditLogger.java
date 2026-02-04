0cb29f8b8d345bb76482219ca5e876b1f020091f
seonjiwon <tommys915@gmail.com>
2026-02-04 14:21:28 +0900

    feature/txn-merge

--- src/log_rolling/txn/TransactionAuditLogger.java
+++ src/log_rolling/txn/TransactionAuditLogger.java
@@ -13,12 +13,13 @@ public class TransactionAuditLogger {
 	}
 
 	public void log(TxnLog txnLog) {
-
+		String maskedAccount = maskAccount(txnLog.getAccountNumber());
+		
 		if (txnLog.getResult().equals("SUCCESS")) {
 			logger.info("txnId={} username={} account={} amount={} result={}", 
 						txnLog.getTxnId(),
 						txnLog.getUsername(), 
-						txnLog.getAccountMasked(), 
+						maskedAccount,  
 						txnLog.getAmount(), 
 						txnLog.getResult()
 					);
@@ -26,11 +27,25 @@ public class TransactionAuditLogger {
 			logger.warn("txnId={} username={} account={} amount={} result={}", 
 					txnLog.getTxnId(),
 					txnLog.getUsername(), 
-					txnLog.getAccountMasked(), 
+					maskedAccount,  
 					txnLog.getAmount(), 
 					txnLog.getResult()
 					);
 		}
 	}
-
+	
+	// 계좌번호 마스킹 처리
+	private String maskAccount(String accountNumber) {
+		if (accountNumber == null || accountNumber.isEmpty()) {
+			return "****";
+		}
+		
+		String[] parts = accountNumber.split("-");
+		// 우리가 기대한 형식과 다르다면
+		if (parts.length != 3) {
+			return "****-****-****";
+		}
+		
+		return parts[0] + "-" + "*".repeat(parts[1].length()) + "-" + parts[2];
+	}
 }
--- src/log_rolling/txn/TxnLog.java
+++ src/log_rolling/txn/TxnLog.java
@@ -1,11 +1,20 @@
 package log_rolling.txn;
 
+
 public class TxnLog {
-	private String txnId; // UUID
-	private String username; // 이미 넣었다고 했으니 OK
-	private long amount; // 랜덤
-	private String accountMasked; // 12****34
-	private String result; // SUCCESS / FAIL
+	private String txnId;
+	private String username;
+	private long amount;
+	private String accountNumber;  
+	private String result; // SUCCECC/FAIL 
+
+	public TxnLog(String txnId, String username, long amount, String accountNumber, String result) {
+		this.txnId = txnId;
+		this.username = username;
+		this.amount = amount;
+		this.accountNumber = accountNumber;
+		this.result = result;
+	}
 
 	public String getTxnId() {
 		return txnId;
@@ -19,22 +28,11 @@ public class TxnLog {
 		return amount;
 	}
 
-	public String getAccountMasked() {
-		return accountMasked;
+	public String getAccountNumber() {
+		return accountNumber;
 	}
 
 	public String getResult() {
 		return result;
 	}
-
-
-	public TxnLog(String txnId, String username, long amount, String accountMasked, String result) {
-		super();
-		this.txnId = txnId;
-		this.username = username;
-		this.amount = amount;
-		this.accountMasked = accountMasked;
-		this.result = result;
-	}
-
 }
